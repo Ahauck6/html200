5fede987fd0e830cec4dbb88f247195410df9e0e
unknown <Ahauck6@uw.edu>
2023-09-22 13:05:32 -0700

    Created variables and operators

---
+++ lesson02/calculator.js
@@ -0,0 +1,14 @@
+let w = 6;
+let x = 8;
+let y = 3;
+let z = 1;
+
+console.log((x - y) * w)
+console.log(x % y)
+console.log(w + y - z)
+console.log(x ** y + z)
+
+console.log((w - z) * y)
+console.log(w % y)
+console.log(x + z - y)
+console.log(w ** y + y) 
