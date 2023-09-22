75d09e06141584a753c1e8247d32de1c0717ab6d
unknown <Ahauck6@uw.edu>
2023-09-22 13:28:30 -0700

    Updated file

--- lesson02/calculator.js
+++ lesson02/calculator.js
@@ -3,12 +3,12 @@ let x = 8;
 let y = 3;
 let z = 1;
 
-console.log((x - y) * w)
-console.log(x % y)
-console.log(w + y - z)
-console.log(x ** y + z)
+console.log((x - y) * w);
+console.log(x % y);
+console.log(w + y - z);
+console.log(x ** y + z);
 
-console.log((w - z) * y)
-console.log(w % y)
-console.log(x + z - y)
-console.log(w ** y + y) 
+console.log((w - z) * y);
+console.log(w % y);
+console.log(x + z - y);
+console.log(w ** y + y); 
