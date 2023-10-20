901e0c9f5c5263d7d306fe3e5a7985d1dbb00f66
unknown <Ahauck6@uw.edu>
2023-10-20 15:46:34 -0700

    Create event handler

---
+++ lesson04/script.js
@@ -0,0 +1,19 @@
+function numberSquared() {
+    let input = prompt ('Enter a number');
+    let result = 0;
+    
+    result = (Number(input))**2;
+    
+    alert(result);
+  }
+
+  //extra challenge
+  function numberMultiplier() {
+    let inputOne = prompt('Enter first number');
+    let inputTwo = prompt('Enter second number');
+    let result = 0
+    
+    result = (Number(inputOne))*(Number(inputTwo));
+    
+    alert(result);
+  }
